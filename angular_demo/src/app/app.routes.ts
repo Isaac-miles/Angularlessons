233553a0260a4839628be48be7cd249b2bac9912
isaac-miles <isaacnice247@gmail.com>
2025-04-15 23:12:52 +0100

    todo-items

--- angular_demo/src/app/app.routes.ts
+++ angular_demo/src/app/app.routes.ts
@@ -4,4 +4,10 @@ export const routes: Routes = [{
     path: '',
     pathMatch: 'full',
     loadComponent: () => import('./home/home.component').then(c => c.HomeComponent)
-}];
+},
+{
+    path: 'todos',
+    pathMatch: 'full',
+    loadComponent: () => import('./todos/todos.component').then(c=> c.TodosComponent)
+}
+];
