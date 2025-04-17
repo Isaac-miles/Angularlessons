e965aa7dbaad5b66ef30262270214869d7b7834e
isaac-miles <isaacnice247@gmail.com>
2025-04-17 16:06:51 +0100

    todo item

--- angular_demo/src/app/todos/todos.component.ts
+++ angular_demo/src/app/todos/todos.component.ts
@@ -3,10 +3,11 @@ import { TodosService } from '../services/todos.service';
 import { ITodo } from '../model/todo.type';
 import { catchError } from 'rxjs';
 import { NgIf } from '@angular/common';
+import { TodoItemComponent } from '../components/todo-item/todo-item.component';
 
 @Component({
   selector: 'app-todos',
-  imports: [NgIf],
+  imports: [NgIf,TodoItemComponent],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
