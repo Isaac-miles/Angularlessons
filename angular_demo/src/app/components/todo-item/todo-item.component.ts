debdc87b10ef620061a113d8b8caebec9551f151
isaac-miles <isaacnice247@gmail.com>
2025-04-17 16:14:05 +0100

    toggle todo

--- angular_demo/src/app/components/todo-item/todo-item.component.ts
+++ angular_demo/src/app/components/todo-item/todo-item.component.ts
@@ -1,12 +1,18 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { ITodo } from '../../model/todo.type';
+import { HighlightCompletedTodoDirective } from '../../directives/highlight-completed-todo.directive';
 
 @Component({
   selector: 'app-todo-item',
-  imports: [],
+  imports: [HighlightCompletedTodoDirective],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.scss'
 })
 export class TodoItemComponent {
-  todo = input.required<ITodo>()
+  todo = input.required<ITodo>();
+  todoToggled = output<ITodo>();
+
+  todoClicked(){
+    
+  }
 }
